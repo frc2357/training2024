84a1346138220d6b1418e31f9e72baa1737c9b5c
Kenneth430 <treygethers@gmail.com>
2024-11-12 20:00:58 -0600

    SmartDashboardSubsystemBase Update

---
+++ lion-turtles-2024/robotbase/src/main/java/frc/robot/subsystems/bases/SmartDashboardSubsystemBase.java
@@ -0,0 +1,22 @@
+package frc.robot.subsystems.bases;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+/** A base for subsystems that log values using {@link SmartDashboard}. */
+public abstract class SmartDashboardSubsystemBase extends SubsystemBase {
+    @Override
+    public void periodic() {
+        if (Constants.USE_SMART_DASHBOARD == true) {
+            smartDashboardPeriodic();
+        }
+    }
+
+    /** 
+     * This method is only called periodically by the {@link CommandScheduler} when {@link Constants#USE_SMART_DASHBOARD} is enabled.
+     * Useful for updating subsystem-specific states with the {@link SmartDashboard} under a special condition.
+    */
+    public void smartDashboardPeriodic() {}
+}
